7df6660b6dcf6736486fb48be6a93628f15b3547
David Lawrence <davi.Lawrence@gmail.com>
2020-03-28 21:03:57 +0100

    (Try to) improved test coverage

--- src/test/kotlin/io/github/hellorin/mongoql/db/variety/ProcessStarterTest.kt
+++ src/test/kotlin/io/github/hellorin/mongoql/db/variety/ProcessStarterTest.kt
@@ -12,10 +12,16 @@ internal class ProcessStarterTest {
         val folder = File(System.getProperty("java.io.tmpdir"))
 
         val osName = System.getProperty("os.name").toLowerCase()
-
-        // TODO find a way to make it work with any platform
         if (osName.startsWith("windows")) {
             val process = processStarter.startAndWaitFor(folder, listOf("cmd.exe", "/c", "ping 8.8.8.8"))
+            assertThat(process).isNotNull
+            assertThat(process.outputStream).isNotNull
+        } else if (osName.contains("mac")) {
+            val process = processStarter.startAndWaitFor(folder, listOf("false"))
+            assertThat(process).isNotNull
+            assertThat(process.outputStream).isNotNull
+        } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
+            val process = processStarter.startAndWaitFor(folder, listOf("false"))
             assertThat(process).isNotNull
             assertThat(process.outputStream).isNotNull
         }
