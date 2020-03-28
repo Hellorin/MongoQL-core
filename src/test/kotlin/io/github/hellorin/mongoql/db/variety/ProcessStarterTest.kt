d61d2c4dc648af377e1e17c77f5e71bb1a3741bf
David Lawrence <davi.Lawrence@gmail.com>
2020-03-28 19:06:14 +0100

    Improved test coverage

---
+++ src/test/kotlin/io/github/hellorin/mongoql/db/variety/ProcessStarterTest.kt
@@ -0,0 +1,30 @@
+package io.github.hellorin.mongoql.db.variety
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class ProcessStarterTest {
+    @Test
+    fun `start process starter`() {
+        val processStarter = ProcessStarter()
+
+        val folder = File(System.getProperty("java.io.tmpdir"))
+
+        val osName = System.getProperty("os.name").toLowerCase()
+        if (osName.startsWith("windows")) {
+            val process = processStarter.startAndWaitFor(folder, listOf("cmd.exe", "/c", "ping 8.8.8.8"))
+            assertThat(process).isNotNull
+            assertThat(process.outputStream).isNotNull
+        } else if (osName.contains("mac")) {
+            val process = processStarter.startAndWaitFor(folder, listOf("ping 8.8.8.8"))
+            assertThat(process).isNotNull
+            assertThat(process.outputStream).isNotNull
+        } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) {
+            val process = processStarter.startAndWaitFor(folder, listOf("ping 8.8.8.8"))
+            assertThat(process).isNotNull
+            assertThat(process.outputStream).isNotNull
+        }
+    }
+}
