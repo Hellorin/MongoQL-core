426b360a06f5e60f4c97de09e0de2d7398aa6ef3
David Lawrence <davi.Lawrence@gmail.com>
2020-04-04 14:43:00 +0200

    Improved tests coverage

--- src/test/kotlin/io/github/hellorin/mongoql/db/variety/MongoVarietyShellExecutorTest.kt
+++ src/test/kotlin/io/github/hellorin/mongoql/db/variety/MongoVarietyShellExecutorTest.kt
@@ -2,30 +2,78 @@ package io.github.hellorin.mongoql.db.variety
 
 import io.github.hellorin.mongoql.db.MongoDBParams
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.Mockito.times
 import java.io.File
 import java.io.InputStream
 import java.io.OutputStream
+import java.lang.IllegalArgumentException
+
 
 internal class MongoVarietyShellExecutorTest {
     @Test
-    fun `test`() {
+    fun `test with no parameters`() {
         // Given
         System.setProperty("os.name", "windows:")
         System.setProperty("java.io.tmpdir", "folder")
 
         val mongoDBParams = MongoDBParams.Builder("db", "col").build()
 
-        val processStarter = object : ProcessStarter() {
-            var isCorrect: Boolean = false
+        val processStarter = getProcess(listOf("variety.cmd", "db/col", "--quiet", "--outputFormat=json"))
+
+        // When
+        MongoVarietyShellExecutor().execute(
+                mongoDBParams,
+                processStarter)
+    }
+
+    @Test
+    fun `test with all parameters`() {
+        // Given
+        System.setProperty("os.name", "windows:")
+        System.setProperty("java.io.tmpdir", "folder")
+
+        val mongoDBParams = MongoDBParams.Builder("db", "col")
+                .username(
+                        "user"
+                ).password(
+                        "password"
+                ).isUsingTLS(
+                        true
+                ).authenticationMechanism(
+                        "SCRAM"
+                ).authenticationDatabase(
+                        "admin"
+                ).host(
+                        "localhost"
+                ).build()
+
+        val processStarter = getProcess(listOf(
+                "variety.cmd",
+                "db/col",
+                "--quiet",
+                "--outputFormat=json",
+                "--host=localhost",
+                "--tls",
+                "--authenticationDatabase=admin",
+                "--authenticationMechanism=SCRAM",
+                "--username=user",
+                "--password=password"))
+
+        // When/Then
+        MongoVarietyShellExecutor().execute(
+                mongoDBParams,
+                processStarter)
+    }
+
+    private fun getProcess(expectedList : List<String>) : ProcessStarter {
+        return object : ProcessStarter() {
 
             override fun startAndWaitFor(parameters: List<String>): Process {
 
-                if (parameters == listOf("variety.cmd", "db/col", "--quiet", "--outputFormat=json")) {
-                    isCorrect = true
-                }
+                if (parameters != expectedList) throw IllegalArgumentException()
 
                 return object : Process() {
                     override fun destroy() {
@@ -55,13 +103,5 @@ internal class MongoVarietyShellExecutorTest {
                 }
             }
         }
-
-        // When
-        MongoVarietyShellExecutor().execute(
-                mongoDBParams,
-                processStarter)
-
-        // Then
-        assertThat(processStarter.isCorrect).isEqualTo(true)
     }
 }
